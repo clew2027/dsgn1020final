f80c8ee8c4fc86f731f770eee565228cc030cd1c
clew2027 <clew27@seas.upenn.edu>
2025-12-14 13:55:16 -0500

    update photo links

--- src/sections.js
+++ src/sections.js
@@ -1,10 +1,20 @@
+import s1a1Preview from "./assets/s1a1_preview.png";
+import s1a2Preview from "./assets/s1a2_preview.png";
+import s1fpPreview from "./assets/s1fp_preview.png";
+import s2a1Preview from "./assets/s2a1_preview.png";
+import s2a2Preview from "./assets/s2a2_preview.png";
+import s2fpPreview from "./assets/s2fp_preview.png";
+import s3a1Preview from "./assets/s3a1_preview.png";
+import s3a2Preview from "./assets/s3a2_preview.png";
+import s3fpPreview from "./assets/s3fp_preview.png";
+
 export const SECTIONS = [
     {
       id: "s1a1",
       title: "Antique ASCII",
       unlocked: false,
       link: "https://example.com/ascii",
-      preview: "src/assets/s1a1_preview.png",
+      preview: s1a1Preview,
       description: "Using ASCII to create web art.",
       readingTitle: "Week 2 - Ulises Carrión - Mail Art and the Big Monster from Second Thoughts",
       discussionQuestions: ["Do you think the “Big Monster” metaphor still applies to today’s digital systems of communication?", "How does shifting the focus from the art piece to the impact change our definition of what counts as art?"]
@@ -14,7 +24,7 @@ export const SECTIONS = [
       title: "CSS Still Life",
       unlocked: false,
       link: "https://example.com/css-still-life",
-      preview: "src/assets/s1a2_preview.png",
+      preview: s1a2Preview,
       description: "A still-life illustration of our DSGN 1020 class created entirely in CSS.",
       readingTitle: "Week 3 - Laurel Schwulst & Édouard U - Selections from Creative Independent and Arena How Do You Use the Internet Mindfully",
       discussionQuestions: ["How might adopting one of Schwult's metaphors for websites (garden, puddle, shelf, etc.)influence how you design or use your own site?", "How can we design websites to be more personal, to foster personal knowledge networks, like what Édouard U. supports?"]
@@ -24,7 +34,7 @@ export const SECTIONS = [
       title: "Kimmy's World",
       unlocked: false,
       link: "https://clew2027.github.io/s1fp/index.html",
-      preview: "src/assets/s1fp_preview.png",
+      preview: s1fpPreview,
       description: "HTML and CSS to show Kimmy.",
       readingTitle: "Week 4 - Olia Lialina - STILL THERE Ruins and Templates of Geocities from Lost and Living (in) Archives",
       discussionQuestions: ["After reading this, what do you think it means for a web page to “age”?", "In what ways might current social media platforms risk repeating the archival fragility and cultural loss seen with Geocities?"]
@@ -34,7 +44,7 @@ export const SECTIONS = [
       title: "Web Ring",
       unlocked: false,
       link: "https://clew2027.github.io/s2fp/index.html",
-      preview: "src/assets/webring.png",
+      preview: s2a1Preview,
       description: "A circular web experience connecting every student's S1FP websites.",
       readingTitle: "Week 5 - Alessandro Ludovico & Florian Cramer - Selections from Publishing from Post-Digital-Print",
       discussionQuestions: ["In what ways does treating print as a collectible or artistic object change its cultural significance compared to its traditional role as an information carrier?","How might future hybrid publishing practices use connectivity and remix culture to push beyond current digital/print limitations?"
@@ -46,7 +56,7 @@ export const SECTIONS = [
       title: "Let's Get a Drink",
       unlocked: false,
       link: "https://clew2027.github.io/s2a2/index.html",
-      preview: "src/assets/s2a2_preview.png",
+      preview: s2a2Preview,
       description: "Practicing interactions with a cool banner",
       readingTitle: "Week 8 - Ramsey Nasser - A Personal Computer for Children of All Cultures from Decolonising the Digital",
       discussionQuestions: ["In what ways does programming language design reinforce or resist cultural and linguistic hierarchies?","What might a truly decolonized computer or programming language look like? Is it even possible?"]
@@ -56,7 +66,7 @@ export const SECTIONS = [
       title: "Interactive Clock",
       unlocked: false,
       link: "https://editor.p5js.org/clew2027/sketches/IZntqWgFt",
-      preview: "src/assets/s2fp_preview.png",
+      preview: s2fpPreview,
       description: "Using p5.js and OOP to create a cool clock.",
       readingTitle: " Week 9 - David Reinfurt - Selections from A *New* Program for Graphic Design",
       discussionQuestions: ["According to Gestalt principles,  how can designers create more intuitive and effective visuals?", "Do you think the way we as humans group information/visuals affect our creativity and expression?"]
@@ -67,7 +77,7 @@ export const SECTIONS = [
       title: "J*b Application",
       unlocked: false,
       link: "https://job-application-kohl-eight.vercel.app/",
-      preview: "src/assets/s3a1_preview.png",
+      preview: s3a1Preview,
       description: "Experimenting with scripts",
       readingTitle: "Week 10 - Ben Duvall - Selections from New Modernism(s)",
       discussionQuestions: ["How does Duvall’s idea of “hypermodernism” change our understanding of what it means for design to be “original” or “authentic”?", "In a world saturated with digital images and hyperlinks, is it still possible for design to create depth or sincerity, or are we permanently bound to the surface?"]
@@ -78,7 +88,7 @@ export const SECTIONS = [
       title: "Keep Up With Philly",
       unlocked: false,
       link: "https://keep-up-w-philly.vercel.app/",
-      preview: "src/assets/s3a2_preview.png",
+      preview: s3a2Preview,
       description: "Events + discovery dashboard for Philly happenings.",
       readingTitle: "Week 12 - Dot Tuer - Beyond the New Media Frame The Poetics of Absence in Vera Frenkel’s String Games",
       discussionQuestions: ["In what ways does String Games anticipate the emotional and philosophical consequences of digital communication we experience today?", "How does Frenkel’s idea of the “poetics of absence” challenge the common assumption that technology enhances human connection?"]
@@ -88,10 +98,9 @@ export const SECTIONS = [
       title: "Space For Chance",
       unlocked: false,
       link: "https://clew2027.github.io/s3fp/index.html",
-      preview: "src/assets/s3fp_preview.png",
+      preview: s3fpPreview,
       description: "Embracing randomness by generating web art",
       readingTitle: "Week 14 - Boris Groys - Art on the Internet",
       discussionQuestions: ["How does the collapse of the boundary between art production and art exhibition on the internet change what we consider an “artwork”?", "Is radical online transparency empowering for artists, or does it ultimately limit creativity by eliminating private space for experimentation?"]
     },
   ];
-  
